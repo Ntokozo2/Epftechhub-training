de19e7cbf4a2fb6e6a249e6f65849ee1ffa5707e
Your Name <you@example.com>
2023-06-12 19:04:24 +0200

    solution

---
+++ problemFour.js
@@ -0,0 +1,30 @@
+function isPalindrome(number){
+    if(typeof number !== 'number') return '${number} is not a number';
+    
+    number = number.toString().split('');
+    
+    for(var i = 0; i < (number.length / 2); i++){
+       if(a[i] !== a[a.length - 1 - i]){
+        return false;
+       } 
+    }
+    
+  return true;
+}
+
+function largestPalindrome(){
+    let largest = 0;
+
+    for(let x = 999; x>= 100; x--){
+      for(let y = 999; y >= 100; y--){
+        let product = x * y;
+        if(isPalindrome(product) && product > largest){
+            largest = product;
+      }
+    }
+
+}
+ return largest;  
+}
+
+console.log(largestPalindrome(999));
